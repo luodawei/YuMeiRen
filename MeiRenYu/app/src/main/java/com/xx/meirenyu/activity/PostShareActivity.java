392daaea3d5f4082b2397cbf4c35805e7b271b9e
luodawei <1078926400.qq.com>
2016-12-08 09:09:29 +0800

    没改什么

--- MeiRenYu/app/src/main/java/com/xx/meirenyu/activity/PostShareActivity.java
+++ MeiRenYu/app/src/main/java/com/xx/meirenyu/activity/PostShareActivity.java
@@ -15,6 +15,7 @@ public class PostShareActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_share);
         btn_back= (ImageView) findViewById(R.id.btn_back);
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,7 +23,5 @@ public class PostShareActivity extends Activity {
                 finish();
             }
         });
-        setContentView(R.layout.activity_post_share);
-
     }
 }
